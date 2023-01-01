f558407575940a1b7b0cbef517944f6609e7c990
Ruteski <lruteski@gmail.com>
2023-01-01 15:26:39 -0300

    path mapping com babel plugin module resolver

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export function Groups() {
    return (
       <View style={styles.container}>
-         <Text>Groups</Text>
+         <Text>Groups!!!</Text>
       </View>
     );
 }
@@ -11,7 +11,7 @@ export function Groups() {
 const styles = StyleSheet.create({
    container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#cecece',
       alignItems: 'center',
       justifyContent: 'center',
    },
